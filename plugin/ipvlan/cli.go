65e4a080884240530af8fa277440630ead21b39d
Brent Salisbury <brent.salisbury@gmail.com>
2015-08-10 06:31:08 -0400

    Added default ns routes for L3 mode and some lint issues

--- plugin/ipvlan/cli.go
+++ plugin/ipvlan/cli.go
@@ -4,7 +4,7 @@ import "github.com/codegangsta/cli"
 
 var (
 	//  Exported user CLI flag config options
-	FlagIpVlanMode     = cli.StringFlag{Name: "mode", Value: ipVlanMode, Usage: "name of the ipvlan mode [l2|l3]. (default: l2)"}
+	FlagIPVlanMode     = cli.StringFlag{Name: "mode", Value: ipVlanMode, Usage: "name of the ipvlan mode [l2|l3]. (default: l2)"}
 	FlagGateway        = cli.StringFlag{Name: "gateway", Value: "", Usage: "IP of the default gateway (defaultL2 mode: first usable address of a subnet. Subnet 192.168.1.0/24 would mean the container gateway to 192.168.1.1)"}
 	FlagSubnet         = cli.StringFlag{Name: "ipvlan-subnet", Value: defaultSubnet, Usage: "subnet for the containers (l2 mode: 192.168.1.0/24)"}
 	FlagMtu            = cli.IntFlag{Name: "mtu", Value: cliMTU, Usage: "MTU of the container interface (default: 1500)"}
@@ -17,5 +17,5 @@ var (
 	ipVlanEthIface = "eth1"           // default to eth0?
 	defaultSubnet  = "192.168.1.0/24" // Should this just be the eth0 IP subnet?
 	gatewayIP      = ""               // GW required for L2. increment network addr+1 if not defined
-	cliMTU = 1500
+	cliMTU         = 1500
 )
